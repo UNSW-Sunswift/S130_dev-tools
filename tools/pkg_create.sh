f2c8e923d90506be41d35fe43c2ba10947a8d9ac
Ryan <wongrt0502@gmail.com>
2025-12-22 19:28:02 +1100

    feat: added initial naive pkg creation logic, no error checks

--- tools/pkg_create.sh
+++ tools/pkg_create.sh
@@ -3,7 +3,7 @@ set -ueo pipefail
 
 ###############################################################################
 # Sunswift High Level DDS Package generator
-# Version: V1
+# Version: V0.1
 # Date: 22/12/2025
 # Author: Ryan Wong
 #
@@ -24,5 +24,26 @@ set -ueo pipefail
 # config -> json files (probably) for node configs + params + choice of launch file
 # launch -> containing one or more launch files
 # logs -> directory for node output logs
+# 
+# Usage:
 ###############################################################################
 
+### Handle command line args and error checks
+if [[ $# -ne 1 ]]; then
+    echo "ERR: Missing parameters"
+    echo "Usage: $0 <package_name>"
+    exit 1
+fi
+
+pkg_name="$1"
+
+if [[ -d "$pkg_name" ]]; then
+    echo "ERR: Package already exists" # will do more error checking later
+    exit 1
+fi
+
+### Create package
+mkdir -p "$pkg_name"/{src,include,config,launch,logs}
+touch "$pkg_name"/{Makefile,README.md}
+
+echo "Package $pkg_name created successfully"
